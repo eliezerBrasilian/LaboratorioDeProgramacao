48048e1772f3f20ceaff4b39b75c6dc6f635df79
Eliezer Assuncao <eliezerassuncaocustodio@gmail.com>
2023-04-29 16:18:43 -0300

    estilizacao

--- CalculadoraDeMedia/index.js
+++ CalculadoraDeMedia/index.js
@@ -1,48 +1,46 @@
 var podeCalcularMedia = true;
-var numeros = []
+var numeros = [];
 
-const btnAdd = document.getElementById('btn-add');
-btnAdd.addEventListener('click',function(ev){
+const btnAdd = document.getElementById("btn-add");
+btnAdd.addEventListener("click", function (ev) {
+  const inputs = document.getElementById("inputs");
+  const newInput = document.createElement("input");
 
-const inputs = document.getElementById('inputs');
-const newInput = document.createElement('input');
+  newInput.id = "number-input";
+  newInput.className = "number-input";
+  newInput.value = "0";
+  newInput.placeholder = "digite um numero";
+  newInput.type = "number";
+  inputs.appendChild(newInput);
 
-newInput.id = "number-input"
-newInput.className = "number-input"
-newInput.value= "0"
-newInput.placeholder= "digite um numero"
-newInput.type = "number"
-inputs.appendChild(newInput)
+  console.log(newInput.value);
+  numeros.push(Number(newInput.value));
 
-console.log(newInput.value)
-numeros.push(Number(newInput.value));
+  if (podeCalcularMedia) exibirButtonCalcularMedia();
+});
 
-if(podeCalcularMedia) exibirButtonCalcularMedia()
-})
+const btnSubmit = document.getElementById("form");
+btnSubmit.addEventListener("submit", function (ev) {
+  ev.preventDefault();
+  const ni = document.querySelectorAll("#number-input");
 
-const btnSubmit = document.getElementById('form');
-btnSubmit.addEventListener('submit',function(ev){
-   
-    ev.preventDefault();
+  for (let i = 0; i < ni.length; i++) {
+    let htmlElemento = ni[i];
+    let valor = htmlElemento.value;
+    console.log(valor);
+    numeros.push(valor);
+  }
+});
 
-    const ni = document.querySelectorAll('#number-input')
-    
-     for(let i = 0; i < ni.length; i++){
-        let htmlElemento = ni[i]
-        let valor = htmlElemento.value
-        console.log(valor)
-        numeros.push(valor);
-     }
-})
+function exibirButtonCalcularMedia() {
+  podeCalcularMedia = false;
 
-function exibirButtonCalcularMedia(){
-    podeCalcularMedia = false;
-
-    const inputs = document.getElementById('btns');
-    const newBtn = document.createElement('input')
-    newBtn.type='submit'
-    newBtn.id="btn-submit"
-    newBtn.className = "btn"
-    newBtn.value="Calcular média"
-    inputs.appendChild(newBtn)
+  const inputs = document.getElementById("btns");
+  const newBtn = document.createElement("input");
+  newBtn.type = "submit";
+  newBtn.id = "btn-submit";
+  newBtn.className = "btn";
+  newBtn.style = "background-color: aliceblue";
+  newBtn.value = "Calcular média";
+  inputs.appendChild(newBtn);
 }
