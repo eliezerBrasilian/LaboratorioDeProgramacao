20bfdcb8c5fdce8f92d045e817e3285e11a6ff21
Eliezer Assuncao <eliezerassuncaocustodio@gmail.com>
2023-05-11 15:27:11 -0300

    pegando dados e enviando para o php

---
+++ Paginacao/process.php
@@ -0,0 +1,15 @@
+<?php
+
+
+
+$id =  $_POST['id'];
+$nome =  $_POST['nome'];
+$telefone =  $_POST['telefone'];
+$email =  $_POST['email'];
+$endereco =  $_POST['endereco'];
+$cidade =  $_POST['cidade'];
+$bairro =  $_POST['bairro'];
+$rua =  $_POST['rua'];
+$numero =  $_POST['numero'];
+
+echo $nome . " " .$endereco;
