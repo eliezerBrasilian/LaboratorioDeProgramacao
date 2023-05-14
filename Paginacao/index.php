3124ca3177474706036c59e17e2b56b729b4636f
Eliezer Assuncao <eliezerassuncaocustodio@gmail.com>
2023-05-14 15:17:51 -0300

    inserindo alunos

---
+++ Paginacao/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+</head>
+
+<body>
+    <div>
+        <form action="process.php" method="post">
+            <label>Nome: </label>
+            <input type="text" id="nome" name="nome" />
+            <label>Telefone: </label>
+            <input type="number" id="telefone" name="telefone" />
+            <label>Email: </label>
+            <input type="email" id="email" name="email" />
+
+            <br>
+            <label for="pet-select">Selecione o endereco:</label>
+            <select name="endereco_ids" id="endereco-select">
+                <option value="" disabled selected>--Selecione uma opção--</option>
+                <option value="1">1 - Belo Horizonte</option>
+                <option value="2">2 - São Paulo</option>
+                <option value="3">3 - Rio de Janeiro</option>
+            </select>
+            <input type="submit" value="Enviar para o banco de dados" />
+        </form>
+    </div>
+</body>
+
+</html>
+
+<!-- Criar um formulário para inserção de dados em um banco de dados, contendo:
+ID - Identificação auto incremento
+Nome - Nome do aluno
+Telefone - Apenas números
+Email - email do aluno, com validação do formato em Javascript.
+Endereco - Chave estrangeira para outra tabela contendo:
+Cidade
+Bairro
+Rua
+Numero -->
--- Paginacao/process.php
+++ Paginacao/process.php
@@ -1,15 +1,67 @@
 <?php
+$host = "localhost";
+$user = "root";
+$password = "";
+$dbname = "pratica7db";
+$port = 3306;
 
+//realizar conexao
+try{
+    $conn = new PDO("mysql:host=$host;port=$port;dbname=" . $dbname,$user,$password);
+   echo "Conexão feita com o banco de dados";
+}catch(PDOException $e){
+    echo "Erro: " . $e->getMessage();
+}
 
-
-$id =  $_POST['id'];
 $nome =  $_POST['nome'];
 $telefone =  $_POST['telefone'];
 $email =  $_POST['email'];
-$endereco =  $_POST['endereco'];
-$cidade =  $_POST['cidade'];
-$bairro =  $_POST['bairro'];
-$rua =  $_POST['rua'];
-$numero =  $_POST['numero'];
+$id_endereco;
 
-echo $nome . " " .$endereco;
+
+if(!empty($_POST['endereco_ids'])) {
+    $selected = $_POST['endereco_ids'];
+    
+    $id_endereco = $selected;
+    echo 'You have chosen: ' . $id_endereco;
+} else {
+    echo 'Please select the value.';
+}
+
+//tb aluno
+/*id	id_endereco	nome	telefone	email	*/
+
+//inserindo os dados acima na tabela
+
+$pdo = new PDO("mysql:host=$host;port=$port;dbname=" . $dbname,$user,$password);
+    $stmt = $pdo->prepare('INSERT INTO tb_aluno 
+    (id_endereco, nome, telefone, email) 
+    VALUES
+    (:id_endereco, :nome, :telefone, :email)');
+
+    $stmt->bindValue(':id_endereco', $id_endereco);
+    $stmt->bindValue(':nome', $nome);
+    $stmt->bindValue(':telefone', $telefone);
+    $stmt->bindValue(':email', $email);
+    $stmt->execute();
+    if ($stmt->rowCount() >= 1) {
+        echo json_encode('Insercao feita com Sucesso');
+    } else {
+        echo json_encode('Falha ao inserir');
+    }
+
+ 
+
+//echo $nome . " " .$endereco;
+
+/*
+ID - Identificação auto incremento
+Nome - Nome do aluno
+Telefone - Apenas números
+Email - email do aluno, com validação do formato em Javascript.
+Endereco - Chave estrangeira para outra tabela contendo:
+Cidade
+Bairro
+Rua
+Numero
+*/
